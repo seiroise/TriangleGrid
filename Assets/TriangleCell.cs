9f64437de68bdcaca0998e28fd21472af56cc84b
seiroise <seiroise@gmail.com>
2019-08-07 08:28:04 +0900

    added text to display coordinate

---
+++ Assets/TriangleCell.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using TMPro;
+
+public sealed class TriangleCell : MonoBehaviour
+{
+    // 左下の頂点から中心に向かうベクトル。
+    private static readonly Vector3 dirToCenter = new Vector3(Mathf.Cos(Mathf.PI / 6f), Mathf.Sin(Mathf.PI / 6f));
+
+    public TextMeshPro text;
+    public Vector3Int coordinate;
+
+    public float size = 0f;
+
+    public Vector3 GetCenterPosition()
+    {
+        return transform.position + transform.rotation * dirToCenter * size * 0.5f;
+    }
+}
